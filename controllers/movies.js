b3bffcb00e1aa4ebfca936bf096a45e9b726ba09
Gabriel Feippe <gabriel@feippe.com>
2025-05-27 19:29:11 -0300

    control error

--- controllers/movies.js
+++ controllers/movies.js
@@ -3,70 +3,95 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getMovies = async (req, res) => {
     //#swagger.tags = ['Movies'];
-    const result = await mongodb.getDatabase().db().collection('movies').find();
-    result.toArray().then((movies) => {
+    try {
+        const result = await mongodb.getDatabase().db().collection('movies').find();
+        result.toArray().then((movies) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(movies);
+        });
+    } catch (error) {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(movies);
-    });
+        res.status(500).json({ error });
+    }
 };
 
 const getMovie = async (req, res) => {
     //#swagger.tags = ['Movies'];
-    const id = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('movies').find({ _id: id });
-    result.toArray().then((movie) => {
+    try {
+        const id = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('movies').find({ _id: id });
+        result.toArray().then((movie) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(movie[0]);
+        });
+    } catch (error) {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(movie[0]);
-    });
+        res.status(500).json({ error });
+    }
 };
 
 const createMovie = async (req, res) => {
     //#swagger.tags = ['Movies'];
-    const movie = {
-        name: req.headers.name,
-        review: req.headers.review,
-        year: req.headers.year,
-        director: req.headers.director,
-        rating: req.headers.rating,
-        duration: req.headers.duration,
-        imdbrating: req.headers.imdbrating
-    };
-    const response = await mongodb.getDatabase().db().collection('movies').insertOne(movie);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while saving the movie');
+    try {
+        const movie = {
+            name: req.headers.name,
+            review: req.headers.review,
+            year: req.headers.year,
+            director: req.headers.director,
+            rating: req.headers.rating,
+            duration: req.headers.duration,
+            imdbrating: req.headers.imdbrating
+        };
+        const response = await mongodb.getDatabase().db().collection('movies').insertOne(movie);
+        if (response.acknowledged) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while saving the movie');
+        }
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error });
     }
 };
 
 const updateMovie = async (req, res) => {
     //#swagger.tags = ['Movies'];
-    const id = new ObjectId(req.params.id);
-    const movie = {
-        name: req.headers.name,
-        review: req.header.review,
-        year: req.headers.year,
-        director: req.headers.director,
-        rating: req.headers.rating,
-        duration: req.headers.duration,
-        imdbrating: req.headers.imdbrating
-    };
-    const response = await mongodb.getDatabase().db().collection('movies').replaceOne({ _id: id }, movie);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the movie');
+    try {
+        const id = new ObjectId(req.params.id);
+        const movie = {
+            name: req.headers.name,
+            review: req.header.review,
+            year: req.headers.year,
+            director: req.headers.director,
+            rating: req.headers.rating,
+            duration: req.headers.duration,
+            imdbrating: req.headers.imdbrating
+        };
+        const response = await mongodb.getDatabase().db().collection('movies').replaceOne({ _id: id }, movie);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the movie');
+        }
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error });
     }
 };
 
 const deleteMovie = async (req, res) => {
     //#swagger.tags = ['Movies'];
-    const id = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('movies').deleteOne({ _id: id });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the movie');
+    try {
+        const id = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('movies').deleteOne({ _id: id });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while deleting the movie');
+        }
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error });
     }
 };
 
--- controllers/songs.js
+++ controllers/songs.js
@@ -3,62 +3,87 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getSongs = async (req, res) => {
     //#swagger.tags = ['Songs'];
-    const result = await mongodb.getDatabase().db().collection('songs').find();
-    result.toArray().then((songs) => {
+    try {
+        const result = await mongodb.getDatabase().db().collection('songs').find();
+        result.toArray().then((songs) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(songs);
+        });
+    } catch (error) {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(songs);
-    });
+        res.status(500).json({ error });
+    }
 };
 
 const getSong = async (req, res) => {
     //#swagger.tags = ['Songs'];
-    const id = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('songs').find({ _id: id });
-    result.toArray().then((song) => {
+    try {
+        const id = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('songs').find({ _id: id });
+        result.toArray().then((song) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(song[0]);
+        });
+    } catch (error) {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(song[0]);
-    });
+        res.status(500).json({ error });
+    }
 };
 
 const createSong = async (req, res) => {
     //#swagger.tags = ['Songs'];
-    const song = {
-        name: req.headers.name,
-        singer: req.headers.singer,
-        year: req.headers.year
-    };
-    const response = await mongodb.getDatabase().db().collection('songs').insertOne(song);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while saving the song');
+    try {
+        const song = {
+            name: req.headers.name,
+            singer: req.headers.singer,
+            year: req.headers.year
+        };
+        const response = await mongodb.getDatabase().db().collection('songs').insertOne(song);
+        if (response.acknowledged) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while saving the song');
+        }
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error });
     }
 };
 
 const updateSong = async (req, res) => {
     //#swagger.tags = ['Songs'];
-    const id = new ObjectId(req.params.id);
-    const song = {
-        name: req.headers.name,
-        singer: req.headers.singer,
-        year: req.headers.year
-    };
-    const response = await mongodb.getDatabase().db().collection('songs').replaceOne({ _id: id }, song);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the song');
+    try {
+        const id = new ObjectId(req.params.id);
+        const song = {
+            name: req.headers.name,
+            singer: req.headers.singer,
+            year: req.headers.year
+        };
+        const response = await mongodb.getDatabase().db().collection('songs').replaceOne({ _id: id }, song);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the song');
+        }
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error });
     }
 };
 
 const deleteSong = async (req, res) => {
     //#swagger.tags = ['Songs'];
-    const id = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('songs').deleteOne({ _id: id });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the song');
+    try {
+        const id = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('songs').deleteOne({ _id: id });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while deleting the song');
+        }
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(500).json({ error });
     }
 };
 
